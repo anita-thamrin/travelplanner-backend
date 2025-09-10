62a7b267281d89ab75af964b07e44dd371238b7a
Anita <anita.thamrin@gmail.com>
2025-09-10 21:49:16 +0800

    use h2 for test

--- src/test/java/com/example/travelplanner/controller/ItineraryControllerTest.java
+++ src/test/java/com/example/travelplanner/controller/ItineraryControllerTest.java
@@ -11,6 +11,7 @@ import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.beans.factory.annotation.Autowired;
 
@@ -23,6 +24,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @WebMvcTest(ItineraryController.class)
 @Import(ItineraryControllerTest.MockServiceConfig.class)
+@ActiveProfiles("test") 
 public class ItineraryControllerTest {
 
     @Autowired
